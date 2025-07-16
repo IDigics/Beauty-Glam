c92eaa8c878db94fda700b37935d8e34d7d1e4d6
YanguiHadil <yanguihadil@gmail.com>
2025-07-17 00:24:25 +0100

    Modification du contexte produit et mise à jour du ProductList

--- app/context/ProductContext.jsx
+++ app/context/ProductContext.jsx
@@ -5,20 +5,48 @@ const ProductContext = createContext()
 
 export function ProductProvider({ children }) {
   const [products, setProducts] = useState([
-    {
-      id: 1,
-      name: "Rouge à lèvres Luxe",
-      price: 45,
-      image: "/products/lipstick.png",
-      description: "Un rouge à lèvres de haute qualité pour un look glamour."
-    },
-    {
-      id: 2,
-      name: "Mascara Volume",
-      price: 35,
-      image: "/products/mascara.png",
-      description: "Mascara pour un regard intense et volumineux."
-    }
+     {
+    id: 1,
+    name: "Rouge à lèvres Luxe",
+    price: 45,
+    image: "/products/lipstick.png",
+    description: "Un rouge à lèvres de haute qualité pour un look glamour."
+  },
+  {
+    id: 2,
+    name: "fards à paupières",
+    price: 80,
+    image: "/products/eyeshadow.png",
+    description: "Des couleurs vibrantes pour sublimer votre regard."
+  },
+  {
+    id: 3,
+    name: "Fond de teint naturel",
+    price: 70,
+    image: "/products/foundation.png",
+    description: "Un fond de teint qui s’adapte parfaitement à votre peau."
+  },
+  {
+    id: 4,
+    name: "Crème Hydratante",
+    price: 80,
+    image: "/products/creme.png",
+    description: "Une crème légère et non grasse qui pénètre rapidement pour hydrater intensément la peau du visage. Convient à tous les types de peau, même sensibles."
+  },
+  {
+    id: 5,
+    name: "Baume à lèvres Luxe",
+    price: 50,
+    image: "/products/baume.png",
+    description: "Un baume à lèvres enrichi en huiles naturelles pour hydrater, adoucir et protéger vos lèvres toute la journée. Idéal pour les lèvres sèches ou gercées."
+  },
+  {
+    id: 6,
+    name: "Mascara Volume Intense",
+    price: 48,
+    image: "/products/mascara.jpeg",
+    description: "Un mascara longue tenue qui apporte un volume spectaculaire à vos cils dès la première application. Sa brosse haute précision sépare et allonge chaque cil sans paquets, pour un regard profond et envoûtant."
+  },
   ])
 
   return (
--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -1,8 +1,12 @@
 'use client'
 
+import { useProduct } from '../app/context/ProductContext'
+
 import ProductCard from './ProductCard'
 
-export default function ProductList({ products }) {
+export default function ProductList() {
+  const { products } = useProduct()
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-4">
       {products.map((product) => (
